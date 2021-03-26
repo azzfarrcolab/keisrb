0da5e46b75a51cf2688be848a4e6ec35bb634820
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-26 09:55:17 +0700

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 reset
 
-./SRBMiner-MULTI --algorithm cryptonight_xhv --pool pool.hashvault.pro:80 --wallet hvxyAVVK3yAMum1KL3dywBMAnecwWvtEQTqFqUEv7nXw4esu8mt9GJ8BDEiBjdq54NYN6ZDQ7SCsHRvpChZcdhyn3CtTojKCXU --password power
+./SRBMiner-MULTI --algorithm cryptonight_xhv --pool ca.haven.miner.rocks:4003 --wallet hvxyAVVK3yAMum1KL3dywBMAnecwWvtEQTqFqUEv7nXw4esu8mt9GJ8BDEiBjdq54NYN6ZDQ7SCsHRvpChZcdhyn3CtTojKCXU --password power
